131d9dd3ac4b78afd49f539b29b944194788db2f
Kojiw21 <Muhammadfauzi7676@gmail.com>
2024-07-30 21:46:47 +0700

    lpk.py

--- lpk.py
+++ lpk.py
@@ -56,7 +56,7 @@ st.markdown("""
         background-color: #51FF4B;
     }
     .main {
-        background-color: #000000;
+        background-color: #808000;
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
